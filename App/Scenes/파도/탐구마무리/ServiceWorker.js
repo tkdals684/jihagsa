7f8170704e3a28cc26057f6867ccd76447b6b8e1
SangMin <tkdals5402@gmail.com>
2022-12-09 10:43:58 +0900

    1

--- App/Scenes/파도/탐구마무리/ServiceWorker.js
+++ App/Scenes/파도/탐구마무리/ServiceWorker.js
@@ -1,6 +1,33 @@
+const cacheName = "JCMediLab-Jihagsa4-1";
+const contentToCache = [
+    "Build/탐구마무리.loader.js",
+    "Build/탐구마무리.framework.js",
+    "Build/탐구마무리.data",
+    "Build/탐구마무리.wasm",
+    "TemplateData/style.css"
+
+];
 
 self.addEventListener('install', function (e) {
     console.log('[Service Worker] Install');
     
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
 });
 
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
